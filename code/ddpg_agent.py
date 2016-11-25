cd8d4dcd9be00e5ff50edcd4e04f07789f323be1
Lucio Dery <ldery@stanford.edu>
2016-11-25 01:30:16 -0800

    added implementation of ddpg agent and updated gym_pipeline

--- code/ddpg_agent.py
+++ code/ddpg_agent.py
@@ -1,26 +1,80 @@
+import numpy as np
+from replay_buffer import *
+import tensorflow as tf
 
-
+TRAIN_BATCH_SIZE = 128 
+DISCOUNT = 0.99
+NOISE_MEAN = 0
+NOISE_STD = 0.01
 
 class DdpgAgent(object):
-    def __init__(self):
-        self.actor_net = ActorNet()
-        self.critic_net = CriticNet()
-        self.replay_buffer = ReplayBuffer(buffer_size=1000000)
+    def __init__(self, env):
+        session = tf.InteractiveSession()
+        self.state_norm_params = (env.observation_space.high, env.observation_space.low)
+        self.action_norm_params = (env.action_space.high, env.action_space.low)
+        self.actor = ActorNet(session, env.observation_space.shape[0],  env.action_space.shape[0])
+        self.critic = CriticNet(session,env.observation_space.shape[0], env.action_space.shape[0])
+        self.replay_buffer = ReplayBuffer()
 
+
+    # Note, for reacher task, the observation space ranges from -inf to inf. Also maybe better to compute mean and diff at start
+    def _normalize_state(self, state):
+        mean = sum(self.state_norm_params)/2
+        diff = (self.state_norm_params[0] - self.state_norm_params[1] ) / 2
+        return (state - mean)/ diff
+
+    def _normalize_action(self, action):
+        mean = sum(self.action_norm_params)/2
+        diff = (self.action_norm_params[0] - self.action_norm_params[1] ) / 2
+        return (action - mean)/diff
+
+    def _normalize_reward(self, reward):
+        # currently does nothing. Consider normalizing reward
+        return reward
+
+    def _un_normalize_action(self, actor_action):
+        mean = sum(self.action_norm_params)/2
+        diff = (self.action_norm_params[0] - self.action_norm_params[1] ) / 2
+        return actor_action*diff + mean
 
     def perceive_and_train(self, s, a, r, s_p):
-        # puts new info into buffer
-        # trains on buffer
+        s = self._normalize_state(s)
+        s_p = self._normalize_state(s_p)
+        a = self._normalize_action(a)
+        r = self._normalize_reward(r)
+        self.replay_buffer.add_observation((s, a, r, s_p))
+        if self.buffer.can_replay():
+            self._train()
 
 
-    def choose_action(self, s):
-        a = self.actor_net.get_action(s)
+    def get_noisy_action(self, s):
+        action = self.get_action(s) + np.random.normal(NOISE_MEAN, NOISE_STD, np.shape(a)) 
+        return action
+
+    def get_action(self, s):
+        a = self.actor.get_action(s)
+        return self._un_normalize_action(a)
 
 
     def _train(self):
-        # implement DDPG Algorithm
-        # go through replay buffer
-        # for the batch, computer loss for the critic
-        # use critic q gradient, pass it into actor,
-        # update target networks (both actor and critic)
+        # Get minibatch
+        states, actions, rewards, next_states = self.replay_buffer.get_minibatch(TRAIN_BATCH_SIZE)
+        
+        # Compute train targets 
+        policy_advantages = self.critic.get_action_gradients((states, actions))
+        target_next_actions = self.actor.compute_target_actions(next_states)
+        target_q_values = self.critic.compute_target_q_value((next_states, target_next_actions))
+        train_targets = rewards + DISCOUNT*target_q_values
+
+        # Train  and update networks 
+        self.critic.train((states, actions, train_targets))
+        self.actor.train((states, policy_advantages))
+        self.critic.update_target()
+        self.actor.update_target()
+        
+
+
+
+
+
 
--- code/gym_pipeline.py
+++ code/gym_pipeline.py
@@ -2,25 +2,40 @@ import gym
 import tensorflow as tf
 from ddpg_agent import *
 
+NUM_TEST_TRIALS = 100
 
 class Pipeline(object):
     def __init__(self, env_name='InvertedPendulum-v1'):
         self.env = gym.make(env_name)
         # get action + observation space, pass into agent
-        self.agent = DdpgAgent()
+        self.agent = DdpgAgent(env)
 
     def _run_episode(self):
-        self.env.reset()
-
+        state = self.env.reset()
         for timestep in xrange(self.env.spec.timestep_limit):
-            action = self.agent.get_action(noise=None)
-            # execute in environment
-            # perceive new information and train
+            action = self.agent.get_noisy_action(state)
+            next_state, reward, done , info = self.env.step(action)
+            self.agent.percieve(state, action, reward, next_state)
+            state = next_state
+            if done:
+                break
 
 
     def run_test(self):
         # run current agent model on environment,
         # evaluate average reward, create video
+        total_reward = 0.0
+        for episode in xrange(NUM_TEST_TRIALS):
+            state = self.env.reset()
+            for step in xrange(self.env.spec.timestep_limit):
+                action = self.agent.get_action(state)
+                next_state, reward, done , info = self.env.step(action)
+                state = next_state
+                total_reward += reward
+                if done:
+                    break
+        avg_reward = total_reward / NUM_TEST_TRIALS
+        print 'Average Reward Per Episode : ', avg_reward
 
 
 def run_training_pipeline(num_episodes=100000):
@@ -28,7 +43,6 @@ def run_training_pipeline(num_episodes=100000):
 
     for episode in xrange(num_episodes):
         pipeline._run_episode()
-
 
         # Every 100 episodes, run test and print average reward
         if episode % 100 == 0:
